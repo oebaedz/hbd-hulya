2368b748ac2be9145f28ef30136edd6dfc9c0b6f
oebaedz <126221249+oebaedz@users.noreply.github.com>
2023-08-17 00:51:16 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,23 +1,23 @@
-// trigger to play music in the background with sweetalert
-// window.addEventListener("load", () => {
-//   Swal.fire({
-//     title: "Mau pake musik ngga, Non?",
-//     // text: "You won't be able to revert this!",
-//     icon: "warning",
-//     showCancelButton: true,
-//     confirmButtonColor: "#3085d6",
-//     cancelButtonColor: "#d33",
-//     confirmButtonText: "Yes",
-//     cancelButtonText: "No",
-//   }).then((result) => {
-//     if (result.isConfirmed) {
-//       document.querySelector(".song").play();
-//       animationTimeline();
-//     } else {
-//       animationTimeline();
-//     }
-//   });
-// });
+trigger to play music in the background with sweetalert
+window.addEventListener("load", () => {
+  Swal.fire({
+    title: "Mau pake musik ngga, Non?",
+    // text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes",
+    cancelButtonText: "No",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      document.querySelector(".song").play();
+      animationTimeline();
+    } else {
+      animationTimeline();
+    }
+  });
+});
 
 // animation timeline
 const animationTimeline = () => {
